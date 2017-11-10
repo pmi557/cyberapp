0fa4e6ed691ec1b98e1d3260f6e3ac0f19d5d0bf
pmi557 <pmujta@gmail.com>
2017-11-10 13:37:49 +0100

    Petla dodana

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,11 +8,19 @@ import java.util.Scanner;
 class CyberStart {
     public static void main(String[] args) {
         String thename;
+        
         System.out.println("Hello World!\n\n");
         System.out.print("What's Your name:") ; 
         Scanner odczyt = new Scanner (System.in);
         thename = odczyt.nextLine();
         System.out.println("Fuck the " +thename);
         System.out.print("Yea'h :)\n");
+
+        int numer = 0;
+        while (numer<10) {
+            System.out.println("Numer"+numer);
+            numer++;
+        };
+        System.out.println("Done.\n");
     }
 }
