2a67b43a52ef5afd421f789469920c818a7081ed
pmi557 <pmujta@gmail.com>
2017-11-10 13:27:59 +0100

    Now is good.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,9 +2,17 @@
  * The HelloWorldApp class implements an application that
  * simply prints "Hello World!" to standard output.
  */
+import java.util.Scanner;
+
+
 class CyberStart {
     public static void main(String[] args) {
-        System.out.println("Hello World!\n\n"); 
-        System.out.println("Fuck the RP system ! YEah .. :) \n");// Display the string.
+        String thename;
+        System.out.println("Hello World!\n\n");
+        System.out.print("What's Your name:") ; 
+        Scanner odczyt = new Scanner (System.in);
+        thename = odczyt.nextLine();
+        System.out.println("Fuck the " +thename);
+        System.out.print("Yea'h :)\n");
     }
 }
